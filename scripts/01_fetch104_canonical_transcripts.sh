93f11de958096a8d2afd3c936351d4a7e0474fe7
jb63 <jb63@sanger.ac.uk>
2025-04-16 09:11:57 +0000

    Bump devcontainer

--- scripts/01_fetch104_canonical_transcripts.sh
+++ scripts/01_fetch104_canonical_transcripts.sh
@@ -1,4 +1,5 @@
 
-docker run -v $PWD:/$PWD \
+docker run -v $PWD:$PWD \
+--workdir $PWD \
 ensemblorg/ensembl-vep:release_104.1 \
 perl scripts/get_canonical_transcripts.pl > results/inputs/feline_104_canonical_transcripts.tsv
